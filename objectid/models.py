96e0455dba97df1144fd164a4d928ae2b9970ecb
tangdy <2012830018@qq.com>
2024-12-31 14:56:21 +0800

    fix: 修改删除CDCID

--- objectid/models.py
+++ objectid/models.py
@@ -4,9 +4,6 @@ from .objectid import ObjectID
 from .objectid import  create_objectid as _create_objectid 
 
 
-DCID = getattr(settings, 'DCID', 0)
-
-
 class ObjectidModel(models.Model):
     '''objectid ID model
     '''
@@ -23,10 +20,4 @@ class ObjectidModel(models.Model):
     def get_id_datetime(self):
         oid = ObjectID(self.id)
         return oid.timestamp
-    
-    def save(self, *args, **kwargs):
-        update_fields = kwargs.get('update_fields', None)
-        if update_fields and not 'cdcid' in update_fields:
-            kwargs['update_fields'].append('cdcid')
-        self.cdcid = DCID        
-        super().save(*args, **kwargs)
+ 
